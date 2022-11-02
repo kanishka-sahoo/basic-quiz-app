a48192a9e3fc981611bfab462bbd65a73b8bde40
kanishka-sahoo <kanishka.sa.hoo@gmail.com>
2022-11-02 13:41:54 +0530

    A

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 '''
-~~~~~INSERT TITLE HERE~~~~~
+Meme Quiz
 Author: 420s
 '''
+from flask import Flask, flash, redirect, render_template, url_for, flash, request, session
 
-def main():
-    pass    # code will go here instead of 'pass'
+app = Flask(__name__)
 
-if __name__ == '__main__':	# checks if program is run as a script or imported as module, and runs only if as script
-    main()
+@app.route("/")  # this sets the route to this page
+def index():
+    return render_template('index.html')    # provide the webpage
+
+if __name__ == "__main__":  # checks if program is run as a script or imported as module, and runs only if as script
+    app.run(debug=True, host='0.0.0.0')   # at 127.0.0.1:5000
