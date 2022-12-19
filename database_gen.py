6a0f9523649d9fc5eaaeb324d08c47a3a7693eb5
kanishka-sahoo <kanishka.sa.hoo@gmail.com>
2022-12-19 19:41:42 +0530

    Added some new code for register and login of users using sqlite database

---
+++ database_gen.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect('accounts.db') 
+c = conn.cursor()
+
+c.execute('''
+            DROP TABLE IF EXISTS users;
+            ''')
+c.execute('''
+          CREATE TABLE IF NOT EXISTS users(
+            username TEXT PRIMARY KEY, 
+            password TEXT NOT NULL,
+            score INTEGER DEFAULT 0,
+            last_played INTEGER DEFAULT 0,
+            acc_created INTEGER DEFAULT 0);
+          ''')
+
+conn.commit()
