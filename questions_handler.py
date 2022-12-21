de718f1c15fcac934b80df649beb529d033e7df8
Bhuvan <bhuvananand2489@gmail.com>
2022-12-21 13:20:26 +0530

    11th line

--- questions_handler.py
+++ questions_handler.py
@@ -2,4 +2,10 @@ from tkinter import *
 from tkinter import ttk
 y = 0
 a = ttk.notebook()
-frame1 = ttk.Frame()
+frame1 = ttk.Frame(a)
+frame2 = ttk.Frame(a)
+frame3 = ttk.Frame(a)
+frame4 = ttk.Frame(a)
+frame5 = ttk.Frame(a)
+
+root = ttk.Frame(a)
